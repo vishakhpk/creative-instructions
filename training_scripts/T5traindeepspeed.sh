7bafe49a05be5a27109dc27dd0118d5748e0acf9
Tuhin Chakrabarty <tuhinjubcse@gmail.com>
2022-01-11 09:12:59 -0500

    Update T5traindeepspeed.sh

--- training_scripts/T5traindeepspeed.sh
+++ training_scripts/T5traindeepspeed.sh
@@ -2,17 +2,15 @@ export BS=12;
 PYTHONPATH=../../../src
 USE_TF=0
 
-deepspeed --num_gpus=4 ./run_translation.py \
-	--model_name_or_path  /local/nlp/temp/poetryT511B0/checkpoint-801 \
-	--output_dir /local/nlp/temp/poetryT511B1 \
-	--evaluation_strategy=steps \
-	--save_strategy=steps \
-	--eval_steps 400 \
-	--save_steps 400 \
+deepspeed --num_gpus=4 ./run_translation_deepspeed.py \
+	--model_name_or_path t5_11b \
+	--output_dir /output/ \
+	--evaluation_strategy=epoch \
+	--save_strategy=epoch \
 	--do_train \
 	--do_eval \
-	--train_file /home/tuhin.chakr/gpt3/poetrynew/train.json \
-	--validation_file /home/tuhin.chakr/gpt3/poetrynew/val.json \
+	--train_file /home/tuhin.chakr/train.json \
+	--validation_file /home/tuhin.chakr/val.json \
 	--learning_rate 1e-3 \
 	--gradient_accumulation_steps 21 \
 	--overwrite_output_dir \
@@ -23,4 +21,4 @@ deepspeed --num_gpus=4 ./run_translation.py \
 	--per_device_eval_batch_size $BS \
 	--source_lang en_XX \
 	--target_lang en_XX \
-	--deepspeed /home/tuhin.chakr/gpt3/transformers/tests/deepspeed/ds_config_zero3_1.json 
+	--deepspeed ds_config_zero3_1.json 
