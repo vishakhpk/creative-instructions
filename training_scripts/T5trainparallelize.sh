464ba40432f11f7fcc77839ddfe89d358bb3c7c2
Tuhin Chakrabarty <tuhinjubcse@gmail.com>
2022-02-18 21:08:25 -0500

    Update T5trainparallelize.sh

--- training_scripts/T5trainparallelize.sh
+++ training_scripts/T5trainparallelize.sh
@@ -22,3 +22,24 @@ python ./run_translation_parallelize.py \
 	--per_device_eval_batch_size $BS \
 	--source_lang en_XX \
 	--target_lang en_XX
+
+
+# python ./run_translation_parallelize.py \
+# 	--model_name_or_path t5-11b \
+# 	--output_dir /output/ \
+# 	--evaluation_strategy=epoch \
+# 	--save_strategy=epoch \
+# 	--do_train \
+# 	--do_eval \
+# 	--train_file /home/tuhin.chakr/train.json \
+# 	--validation_file /home/tuhin.chakr/validation.json \
+# 	--learning_rate 1e-4 \
+# 	--overwrite_output_dir \
+# 	--max_source_length 1024 \
+# 	--max_target_length 256 \
+# 	--num_train_epochs 1 \
+# 	--gradient_accumulation_steps 256 \
+# 	--per_device_train_batch_size $BS \
+# 	--per_device_eval_batch_size $BS \
+# 	--source_lang en_XX \
+# 	--target_lang en_XX
