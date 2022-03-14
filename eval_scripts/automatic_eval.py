59d88b8a5f225d7edd5ab7da22b69ec2253c4ae4
Vishakh Padmakumar <vishakhpadmakumar@gmail.com>
2022-03-14 15:01:54 -0400

    Updated test sets + automatic eval

--- eval_scripts/automatic_eval.py
+++ eval_scripts/automatic_eval.py
@@ -12,7 +12,8 @@ eval_type = sys.argv[2]
 if eval_type == "subject":
     subj = []
     gen = []
-
+    tot = 0
+    succ = 0
     with open(filename, 'r') as f:
         for line in f:
             obj = json.loads(line.strip())
@@ -20,21 +21,35 @@ if eval_type == "subject":
             t = re.findall("about '([^']*)'", src)
             t2 = re.findall("word '([^']*)'", src)
             t3 = re.findall("ending in '([^']*)'", src)
-            t = t + t2 + t3
+            t4 = re.findall("contains '([^']*)'", src)
+            t = t + t2 + t3 +t4
             t = list(set(t))
+            # print(src, t)
             if len(t) == 0:
                 continue
+            # print("Proceeding")
             temp = ""
+            found_all = True
             for i in t:
                 temp+=i+' '
+                # print(i, i in obj['generation'], t)
+                if i.lower() not in obj['generation'].lower():
+                    # print("Found")
+                    found_all = False
+            # print(t, found_all, src, obj['generation'])
+            tot+=1
+            if found_all:
+                succ+=1
             subj.append(temp)
             gen.append(obj['generation'])
     
     rouge = load_metric("rouge")
     results = rouge.compute(predictions = gen, references = subj)
-    print('Rouge 1', results['rouge1'])
-    print('Rouge 2', results['rouge2'])
-    print('Rouge L', results['rougeL'])
+    print("Total match % ", float(succ)/tot, succ, tot)
+    print('Rouge 1', results['rouge1'].mid)
+    print('Rouge 2', results['rouge2'].mid)
+    print('Rouge L', results['rougeL'].mid)
+
 
 elif eval_type == "rhyme":
     # gen = []
@@ -68,7 +83,7 @@ elif eval_type == "rhyme":
                 # ch = input()
             tot+=1
 
-    print("Percentage rhyming: ", float(succ)/tot, tot, succ)
+    print("Percentage rhyming: ", float(succ)/tot, succ, tot, tot, succ)
     #ch = input()
 elif eval_type == "metaphor":
     succ = 0
@@ -100,7 +115,7 @@ elif eval_type == "metaphor":
             #    print(t, src, gen)
                 continue
                 # ch = input()
-    print("Percentage with subject and comparator: ", float(succ)/tot, tot, succ)
+    print("Percentage with subject and comparator: ", float(succ)/tot, succ, tot, tot, succ)
 
 elif eval_type == "simile":
     succ = 0
@@ -130,13 +145,13 @@ elif eval_type == "simile":
             tot+=1
             if subj and comp:
                 succ += 1
-            elif comp:
+            if comp:
                 comp_ct += 1
             else:
             #    print(t, src, gen)
                 continue
                 # ch = input()
-    print("Percentage with subject and comparator: ", float(succ)/tot, tot, succ)
+    print("Percentage with subject and comparator: ", float(succ)/tot, succ, tot, tot, succ)
     print("Percentage with comparator: ", float(comp_ct)/tot, tot, comp_ct)
 
 elif eval_type == "haiku":
@@ -188,3 +203,91 @@ elif eval_type == "haiku":
 
     stat = st.binned_statistic(counts, counts, bins = 10, statistic = 'count')
     print(stat)
+elif eval_type == "ending":
+    subj = []
+    gen = []
+    tot = 0
+    succ = 0
+    ending = 0
+    with open(filename, 'r') as f:
+        for line in f:
+            obj = json.loads(line.strip())
+            src = obj['instruction']
+            t3 = re.findall("ending in '([^']*)'", src)
+            t = t3
+            t = list(set(t))
+            if len(t) == 0:
+                continue
+            temp = ""
+            found_all = True
+            for i in t:
+                temp+=i+' '
+                if i.lower() not in obj['generation'].lower():
+                    # print(i, i in obj['generation'], t)
+                    found_all = False
+            # print(t, found_all, src, obj['generation'])
+            tot+=1
+            if found_all:
+                succ+=1
+            temp = obj['generation'].replace(".", "")
+            temp = temp.replace(",", "")
+            temp = temp.split()
+            if t[0].lower() == temp[-1].lower():
+                ending+=1
+            # else:
+            #    print("Failed End Test")
+            subj.append(temp)
+            gen.append(obj['generation'])
+    
+    rouge = load_metric("rouge")
+    results = rouge.compute(predictions = gen, references = subj)
+    print("Total match accuracy: ", float(succ)/tot, succ, tot)
+    print("Total end accuracy: ", float(ending)/tot)
+    print('Rouge 1', results['rouge1'].mid)
+    print('Rouge 2', results['rouge2'].mid)
+    print('Rouge L', results['rougeL'].mid)
+
+elif eval_type == "starting":
+    subj = []
+    gen = []
+    tot = 0
+    succ = 0
+    starting = 0
+    with open(filename, 'r') as f:
+        for line in f:
+            obj = json.loads(line.strip())
+            src = obj['instruction']
+            t3 = re.findall("starts with the word '([^']*)'", src)
+            t = t3
+            t = list(set(t))
+            if len(t) == 0:
+                continue
+            temp = ""
+            found_all = True
+            for i in t:
+                temp+=i+' '
+                if i.lower() not in obj['generation'].lower():
+                    # print(i, i in obj['generation'], t)
+                    found_all = False
+            tot+=1
+            if found_all:
+                succ+=1
+            # print(t, found_all, src, obj['generation'])
+            temp = obj['generation'].replace(".", "")
+            temp = temp.replace(",", "")
+            temp = temp.split()
+            if t[0].lower() == temp[0].lower():
+                starting+=1
+            # else:
+            #    print("Failed Start Test")
+            subj.append(temp)
+            gen.append(obj['generation'])
+    
+    rouge = load_metric("rouge")
+    results = rouge.compute(predictions = gen, references = subj)
+    print("Total match accuracy: ", float(succ)/tot, succ, tot)
+    print("Total start accuracy: ", float(starting)/tot)
+    print('Rouge 1', results['rouge1'].mid)
+    print('Rouge 2', results['rouge2'].mid)
+    print('Rouge L', results['rougeL'].mid)
+
