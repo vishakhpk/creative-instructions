0669563f5cd61a062a13208b20c925d522cd7320
Tuhin Chakrabarty <tuhinjubcse@gmail.com>
2022-03-17 17:25:49 -0400

    Add files via upload

--- demo_poetic/demo_instruction.py
+++ demo_poetic/demo_instruction.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template
 from transformers import AutoTokenizer,AutoModelForSeq2SeqLM, T5ForConditionalGeneration
 import os, sys,random
 import yake
+import pronouncing
 import re
 
 os.environ["CUDA_VISIBLE_DEVICES"]="0,1"
@@ -75,12 +76,27 @@ def get_translation(topic,startword,endword,lang,poemsofar):
 	elif lang=="endwithrhyme":
 		last_sentence = re.sub(r'[^\w\s]', '', poemsofar[-1])
 		last_word = last_sentence.split()[-1]
-		instruction = random.choice(["Write a poetic sentence that ends in a word which rhymes with '"+last_word+"'","Generate a poetic sentence that ends in a word which rhymes with '"+last_word+"'"])
+		last_word = random.choice(pronouncing.rhymes(last_word)[0:10])
+		instruction = random.choice(["Write a poetic sentence ending in '"+last_word+"'","Generate a poetic sentence ending in '"+last_word+"'"])
 		print(instruction)
 		inputs = instructiontokenizer(instruction, return_tensors="pt").input_ids
 		sample_outputs = instructionmodel.generate(input_ids=inputs.cuda(), no_repeat_ngram_size=2, num_return_sequences = 5, do_sample=True, max_length=64, top_k=5,temperature=0.7,eos_token_id=instructiontokenizer.eos_token_id)
 		output = instructiontokenizer.batch_decode(sample_outputs, skip_special_tokens=True)
 		return output
+	elif lang=='comp1':
+		print("Topic is ",topic)
+		instruction = random.choice(["Write a poetic sentence that contains the word '"+topic+"' and ending in '"+endword+"'", "Write a poetic sentence that includes the word '"+topic+"' and ending in '"+endword+"'", "Write a poetic sentence about '"+topic+"' and ending in '"+endword+"'"])
+		print(instruction)
+		inputs = instructiontokenizer(instruction, return_tensors="pt").input_ids
+		sample_outputs = instructionmodel.generate(input_ids=inputs.cuda(), no_repeat_ngram_size=2, num_return_sequences = 5, do_sample=True, max_length=64, top_k=5,temperature=0.7,eos_token_id=instructiontokenizer.eos_token_id)
+		output = instructiontokenizer.batch_decode(sample_outputs, skip_special_tokens=True)
+	elif lang=='comp2':
+		startword = startword.capitalize()
+		instruction = random.choice(["Write a poetic sentence that starts with the word '"+startword+"' and ending in '"+endword+"'", "Generate a poetic sentence that starts with the word '"+startword+"' and ending in '"+endword+"'"])
+		print(instruction)
+		inputs = instructiontokenizer(instruction, return_tensors="pt").input_ids
+		sample_outputs = instructionmodel.generate(input_ids=inputs.cuda(), no_repeat_ngram_size=2, num_return_sequences = 5, do_sample=True, max_length=64, top_k=5,temperature=0.7,eos_token_id=instructiontokenizer.eos_token_id)
+		output = instructiontokenizer.batch_decode(sample_outputs, skip_special_tokens=True)	
 
 if __name__ == '__main__':
 	instructiontokenizer = AutoTokenizer.from_pretrained("/mnt/nlp_swordfish/tuhin/poetryT511bcheckpoints/epoch3") 
