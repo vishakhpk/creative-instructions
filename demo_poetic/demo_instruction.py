3239558ae51fd5117e2170a16f7e736cb8df4acf
Vishakh Padmakumar <vishakhpadmakumar@gmail.com>
2022-04-12 21:39:09 -0400

    Cleaning up files

--- demo_poetic/demo_instruction.py
+++ demo_poetic/demo_instruction.py
@@ -40,14 +40,14 @@ def home():
 		if "poemsofar" in form:
 			poem_lines = form["poemsofar"].split('\n')
 		else:
-			poem_lines =  ['a', 'b']
+			poem_lines =  [] # ['a', 'b']
 		return render_template("instruction.html", poem=poem_lines, translation=get_translation(topic, startword, endword, rhymewithword, lang, poem_lines, nl_inst))
 
 	return render_template("index1.html", f='')
 
 def get_translation(topic, startword, endword, rhymewithword, lang, poemsofar, nl_inst):
 	print("NLinst",nl_inst)
-	return ['a', 'b', 'c', 'd', 'e']
+	# return ['a', 'b', 'c', 'd', 'e']
 	if (nl_inst is not None) and (lang not in ['endwithrhyme','comp3','comp4','suggesttopic']):
 		instruction = nl_inst
 		print("I AM HERE......")
